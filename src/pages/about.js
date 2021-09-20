088b4a5a5f0c8dfff43f499c4667e87b83876530
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-09-20 13:50:43 -0700

    add and style layout components

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      </Layout>
+  )
+}
+
+export default AboutPage
