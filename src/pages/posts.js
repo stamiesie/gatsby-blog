edd5d7b1c3e7760849462524a848f44d70e6a044
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-09-20 16:36:39 -0700

    add transformer, MDX plugin

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,32 +1,40 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
-        <Layout pageTitle="Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
-        </Layout>
+        <Layout pageTitle="Posts">
+        {
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <MDXRenderer>
+                  {node.body}
+              </MDXRenderer>
+            </article>
+          ))
+        }
+      </Layout>
 
     )
 }
 
 export const query = graphql`
-    query {
-        allFile {
-            nodes {
-                name
-            }
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        body
+      }
     }
-  `
+  }
+`
 
 export default BlogPage
